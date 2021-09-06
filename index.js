0a521352c66dbc5e35fdbf56176acd63344f13b7
Carlos Martínez <cmr@MacBook-Pro-de-Carlos.local>
2021-09-05 21:52:15 -0500

    Form completed, add video to home and styles

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 const router = express.Router();
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, '/public')))
 
 router.get('/',function(req, res){
   res.sendFile(path.join(__dirname+'/views/index.html'));
